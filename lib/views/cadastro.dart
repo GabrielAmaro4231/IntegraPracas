9281889352130eb4392539c809b262417a391182
CaioAndreta <75509351+CaioAndreta@users.noreply.github.com>
2021-05-26 13:39:12 -0300

    aplicacao do mvc

---
+++ lib/views/cadastro.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Cadastro extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Cadastro')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          
+        ],)
+    );
+  }
+}
---
+++ lib/views/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:integrapracas/views/cadastro.dart';
+
+class App extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Cadastro(),
+    );
+  }
+}
